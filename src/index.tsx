dacfe5267296785d7ba44c97cd3dc7ae43595a77
umid <ubulkasimov@rvision.ru>
2022-10-31 15:03:51 +0500

    sprint-6/step-1
    правки base href

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
     <Provider store={store}>
-        <Router>
+        <Router basename="/react-burger">
             <App />
         </Router>
     </Provider>
